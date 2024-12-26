f19a8dfd1a4a7cd9167c01e7be01f65ce6407a3f
Sithija Shehara <nsithijashehara@gmail.com>
2024-12-26 21:55:02 +0530

    app assert

---
+++ api/src/utils/AppError.ts
@@ -0,0 +1,14 @@
+import AppErrorCode from "../constants/appErrorCode";
+import { HttpStatusCode } from "../constants/http";
+
+export class AppError extends Error {
+  constructor(
+    public statusCode: HttpStatusCode,
+    public message: string,
+    public errorCode?: AppErrorCode
+  ) {
+    super(message);
+  }
+}
+
+export default AppError;
