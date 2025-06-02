83b281c91f2483c2c68b139bbba4218c8a2bdd31
Sithija Shehara <nsithijashehara@gmail.com>
2025-06-02 22:14:34 +0530

    sign in & sign up pages UI

--- client/src/pages/auth/sign-in.tsx
+++ client/src/pages/auth/sign-in.tsx
@@ -1,3 +1,49 @@
-export const SignIn = () => {
-  return <div>Sign In</div>;
+import { Button, Input, Label } from "@/atoms";
+import { cn } from "@/lib/utils";
+import { FORGOT_PASSWORD, SIGN_UP } from "@/router";
+import { Link } from "react-router-dom";
+
+type IProps = {
+  className?: string;
+  props?: any;
+};
+
+export const SignIn = ({ className, props }: IProps) => {
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold"> Sign in to your account</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter your email below to sign in to your account
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+            <Link
+              to={FORGOT_PASSWORD}
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Forgot your password?
+            </Link>
+          </div>
+          <Input id="password" type="password" required />
+        </div>
+        <Button type="submit" className="w-full bg-blue-800 hover:bg-blue-700">
+          Sign in
+        </Button>
+      </div>
+      <div className="text-center text-sm">
+        Don&apos;t have an account?{" "}
+        <Link to={SIGN_UP} className="underline underline-offset-4">
+          Sign up
+        </Link>
+      </div>
+    </form>
+  );
 };
--- client/src/pages/auth/sign-up.tsx
+++ client/src/pages/auth/sign-up.tsx
@@ -1,3 +1,67 @@
-export const SignUp = () => {
-  return <div>Sign Up</div>;
+import { Button, Input, Label } from "@/atoms";
+import { cn } from "@/lib/utils";
+import { SIGN_IN } from "@/router";
+import { Link } from "react-router-dom";
+
+type IProps = {
+  className?: string;
+  props?: any;
+};
+
+export const SignUp = ({ className, props }: IProps) => {
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Sign up to your account</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter your email below to sign up to your account
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="username">Username</Label>
+          <Input id="username" type="text" placeholder="user" required />
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+            {/* <a
+              href="#"
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Forgot your password?
+            </a> */}
+          </div>
+          <Input
+            id="password"
+            type="password"
+            placeholder="*********"
+            required
+          />
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="confirm-password">Confirm Password</Label>
+          <Input
+            id="confirm-password"
+            type="password"
+            placeholder="*********"
+            required
+          />
+        </div>
+        <Button type="submit" className="w-full bg-blue-800 hover:bg-blue-700">
+          Sign up
+        </Button>
+      </div>
+      <div className="text-center text-sm">
+        Already have an account?{" "}
+        <Link to={SIGN_IN} className="underline underline-offset-4">
+          Sign in
+        </Link>
+      </div>
+    </form>
+  );
 };
